432c7c3cb0faecd9a4e9515c1c7668988db91611
datalanetech <datalanetech@gmail.com>
2022-01-09 20:41:23 +0530

    Added values js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/navbar";
 
 import React, { Component } from "react";
+import Values from "./components/Values";
 class App extends Component {
   render(){
     return (      
@@ -63,47 +64,7 @@ class App extends Component {
           </section>
       
           
-          <section id="values" className="values">
-      
-            <div className="container" data-aos="fade-up">
-      
-              <header className="section-header">
-                <h2>Our Values</h2>
-                <p>Odit est perspiciatis laborum et dicta</p>
-              </header>
-      
-              <div className="row">
-      
-                <div className="col-lg-4" data-aos="fade-up" data-aos-delay="200">
-                  <div className="box">
-                    <img src="assets/img/values-1.png" className="img-fluid" alt="" />
-                    <h3>Ad cupiditate sed est odio</h3>
-                    <p>Eum ad dolor et. Autem aut fugiat debitis voluptatem consequuntur sit. Et veritatis id.</p>
-                  </div>
-                </div>
-      
-                <div className="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="400">
-                  <div className="box">
-                    <img src="assets/img/values-2.png" className="img-fluid" alt="" />
-                    <h3>Voluptatem voluptatum alias</h3>
-                    <p>Repudiandae amet nihil natus in distinctio suscipit id. Doloremque ducimus ea sit non.</p>
-                  </div>
-                </div>
-      
-                <div className="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="600">
-                  <div className="box">
-                    <img src="assets/img/values-3.png" className="img-fluid" alt="" />
-                    <h3>Fugit cupiditate alias nobis.</h3>
-                    <p>Quam rem vitae est autem molestias explicabo debitis sint. Vero aliquid quidem commodi.</p>
-                  </div>
-                </div>
-      
-              </div>
-      
-            </div>
-      
-          </section>
-      
+          <Values/>      
           
           <section id="counts" className="counts">
             <div className="container" data-aos="fade-up">
@@ -114,7 +75,7 @@ class App extends Component {
                   <div className="count-box">
                     <i className="bi bi-emoji-smile"></i>
                     <div>
-                      <span data-purecounter-start="0" data-purecounter-end="1" data-purecounter-duration="1" className="purecounter"></span>
+                      <span data-purecounter-start="0" data-purecounter-end="1" data-purecounter-duration="1" className="purecounter"/>
                       <p>Happy Clients</p>
                     </div>
                   </div>
@@ -786,7 +747,7 @@ class App extends Component {
           </section>
       
           
-          <section id="testimonials" className="testimonials">
+          {/* <section id="testimonials" className="testimonials">
       
             <div className="container" data-aos="fade-up">
       
@@ -884,7 +845,7 @@ class App extends Component {
       
             </div>
       
-          </section>
+          </section> */}
       
           
           <section id="team" className="team">
---
+++ src/components/Values.js
@@ -0,0 +1,52 @@
+import React,{Component} from "react";
+
+class Values extends Component{
+    render(){
+        return(
+            <>
+                <section id="values" className="values">
+                
+                    <div className="container" data-aos="fade-up">
+
+                        <header className="section-header">
+                        <h2>Our Values</h2>
+                        <p>Odit est perspiciatis laborum et dicta</p>
+                        </header>
+
+                        <div className="row">
+
+                        <div className="col-lg-4" data-aos="fade-up" data-aos-delay="200">
+                            <div className="box">
+                            <img src="assets/img/values-1.png" className="img-fluid" alt="" />
+                            <h3>Ad cupiditate sed est odio</h3>
+                            <p>Eum ad dolor et. Autem aut fugiat debitis voluptatem consequuntur sit. Et veritatis id.</p>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="400">
+                            <div className="box">
+                            <img src="assets/img/values-2.png" className="img-fluid" alt="" />
+                            <h3>Voluptatem voluptatum alias</h3>
+                            <p>Repudiandae amet nihil natus in distinctio suscipit id. Doloremque ducimus ea sit non.</p>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="600">
+                            <div className="box">
+                            <img src="assets/img/values-3.png" className="img-fluid" alt="" />
+                            <h3>Fugit cupiditate alias nobis.</h3>
+                            <p>Quam rem vitae est autem molestias explicabo debitis sint. Vero aliquid quidem commodi.</p>
+                            </div>
+                        </div>
+
+                        </div>
+
+                    </div>
+
+                </section>
+
+            </>
+        )
+    }
+}
+export default Values; 
