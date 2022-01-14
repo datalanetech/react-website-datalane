5a79f94d13cc37fb067e9f3a32ede820fd1f0324
rkreddy99 <krishna.reddy@iitgn.ac.in>
2022-01-14 21:03:02 +0530

    reactify service cards

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Navbar from './components/navbar';
 
 import React, { Component } from 'react';
 import Values from './components/Values';
+import Card from './components/Card';
+
 class App extends Component {
 	render() {
 		return (
@@ -394,89 +396,47 @@ class App extends Component {
 							</header>
 
 							<div className='row gy-4'>
-								<div className='col-lg-4 col-md-6' data-aos='fade-up' data-aos-delay='200'>
-									<div className='service-box blue'>
-										<i className='ri-discuss-line icon'></i>
-										<h3>Nesciunt Mete</h3>
-										<p>
-											Provident nihil minus qui consequatur non omnis maiores. Eos accusantium minus
-											dolores iure perferendis tempore et consequatur.
-										</p>
-										<a href='#' className='read-more'>
-											<span>Read More</span> <i className='bi bi-arrow-right'></i>
-										</a>
-									</div>
-								</div>
+								<Card
+									title='Eosle Commodi'
+									color='blue'
+									description='Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero
+											corrupti neque eum hic non ut nesciunt dolorem.'
+								/>
 
-								<div className='col-lg-4 col-md-6' data-aos='fade-up' data-aos-delay='300'>
-									<div className='service-box orange'>
-										<i className='ri-discuss-line icon'></i>
-										<h3>Eosle Commodi</h3>
-										<p>
-											Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero
-											corrupti neque eum hic non ut nesciunt dolorem.
-										</p>
-										<a href='#' className='read-more'>
-											<span>Read More</span> <i className='bi bi-arrow-right'></i>
-										</a>
-									</div>
-								</div>
+								<Card
+									title='Eosle Commodi'
+									color='orange'
+									description='Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero
+											corrupti neque eum hic non ut nesciunt dolorem.'
+								/>
 
-								<div className='col-lg-4 col-md-6' data-aos='fade-up' data-aos-delay='400'>
-									<div className='service-box green'>
-										<i className='ri-discuss-line icon'></i>
-										<h3>Ledo Markt</h3>
-										<p>
-											Ut excepturi voluptatem nisi sed. Quidem fuga consequatur. Minus ea aut. Vel
-											qui id voluptas adipisci eos earum corrupti.
-										</p>
-										<a href='#' className='read-more'>
-											<span>Read More</span> <i className='bi bi-arrow-right'></i>
-										</a>
-									</div>
-								</div>
+								<Card
+									title='Ledo Markt'
+									color='green'
+									description='Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero
+											corrupti neque eum hic non ut nesciunt dolorem.'
+								/>
 
-								<div className='col-lg-4 col-md-6' data-aos='fade-up' data-aos-delay='500'>
-									<div className='service-box red'>
-										<i className='ri-discuss-line icon'></i>
-										<h3>Asperiores Commodi</h3>
-										<p>
-											Non et temporibus minus omnis sed dolor esse consequatur. Cupiditate sed error
-											ea fuga sit provident adipisci neque.
-										</p>
-										<a href='#' className='read-more'>
-											<span>Read More</span> <i className='bi bi-arrow-right'></i>
-										</a>
-									</div>
-								</div>
+								<Card
+									title='Ledo Markt'
+									color='red'
+									description='Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero
+											corrupti neque eum hic non ut nesciunt dolorem.'
+								/>
 
-								<div className='col-lg-4 col-md-6' data-aos='fade-up' data-aos-delay='600'>
-									<div className='service-box purple'>
-										<i className='ri-discuss-line icon'></i>
-										<h3>Velit Doloremque.</h3>
-										<p>
-											Cumque et suscipit saepe. Est maiores autem enim facilis ut aut ipsam corporis
-											aut. Sed animi at autem alias eius labore.
-										</p>
-										<a href='#' className='read-more'>
-											<span>Read More</span> <i className='bi bi-arrow-right'></i>
-										</a>
-									</div>
-								</div>
+								<Card
+									title='Ledo Markt'
+									color='purple'
+									description='Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero
+											corrupti neque eum hic non ut nesciunt dolorem.'
+								/>
 
-								<div className='col-lg-4 col-md-6' data-aos='fade-up' data-aos-delay='700'>
-									<div className='service-box pink'>
-										<i className='ri-discuss-line icon'></i>
-										<h3>Dolori Architecto</h3>
-										<p>
-											Hic molestias ea quibusdam eos. Fugiat enim doloremque aut neque non et
-											debitis iure. Corrupti recusandae ducimus enim.
-										</p>
-										<a href='#' className='read-more'>
-											<span>Read More</span> <i className='bi bi-arrow-right'></i>
-										</a>
-									</div>
-								</div>
+								<Card
+									title='Ledo Markt'
+									color='pink'
+									description='Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero
+											corrupti neque eum hic non ut nesciunt dolorem.'
+								/>
 							</div>
 						</div>
 					</section>
---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Card = ({ color, title, description }) => {
+	return (
+		<div className='col-lg-4 col-md-6' data-aos='fade-up' data-aos-delay='700'>
+			<div className={`service-box ${color}`}>
+				<i className='ri-discuss-line icon'></i>
+				<h3>{title}</h3>
+				<p>{description}</p>
+				<a href='#' className='read-more'>
+					<span>Read More</span> <i className='bi bi-arrow-right'></i>
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default Card;
