3347a8f2ed1f43e2fe914db8b34ba52e2220a2dd
datalanetech <datalanetech@gmail.com>
2022-01-15 13:32:05 +0530

    modified pricing component to its own react component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import Navbar from './components/navbar';
 
 import React, { Component } from 'react';
 import Values from './components/Values';
-import ServiceCard from './components/ServiceCard';
-<<<<<<< HEAD
 import FeatureTile from './components/FeatureTile';
 import BlogCard from './components/BlogCard';
-=======
 import ServiceComponent from './components/ServiceComponent';
-import TestimonialsComponent from './components/TestimonialsComponent';
-import TeamComponent from './components/TeamComponent';
-import BlogPosts from './components/BlogPosts';
-import PortfolioComponent from './components/PortfolioComponent';
+// import TestimonialsComponent from './components/TestimonialsComponent';
+// import TeamComponent from './components/TeamComponent';
+// import BlogPosts from './components/BlogPosts';
+// import PortfolioComponent from './components/PortfolioComponent';
 import Footer from './components/Footer';
->>>>>>> b44924e (Added content and moved few unwanted components into another react component)
+// import PricingComponent from './components/PricingComponent';
 
 class App extends Component {
 	render() {
@@ -363,87 +360,7 @@ class App extends Component {
 
 					<ServiceComponent/>
 
-					{/* // <! -- <section id="pricing" className="pricing">
-      
-            <div className="container" data-aos="fade-up">
-      
-              <header className="section-header">
-                <h2>Pricing</h2>
-                <p>Check our Pricing</p>
-              </header>
-      
-              <div className="row gy-4" data-aos="fade-left">
-      
-                <div className="col-lg-3 col-md-6" data-aos="zoom-in" data-aos-delay="100">
-                  <div className="box">
-                    <h3 style="color: #07d5c0;">Free Plan</h3>
-                    <div className="price"><sup>$</sup>0<span> / mo</span></div>
-                    <img src="assets/img/pricing-free.png" className="img-fluid" alt="" />
-                    <ul>
-                      <li>Aida dere</li>
-                      <li>Nec feugiat nisl</li>
-                      <li>Nulla at volutpat dola</li>
-                      <li className="na">Pharetra massa</li>
-                      <li className="na">Massa ultricies mi</li>
-                    </ul>
-                    <a href="#" className="btn-buy">Buy Now</a>
-                  </div>
-                </div>
-      
-                <div className="col-lg-3 col-md-6" data-aos="zoom-in" data-aos-delay="200">
-                  <div className="box">
-                    <span className="featured">Featured</span>
-                    <h3 style="color: #65c600;">Starter Plan</h3>
-                    <div className="price"><sup>$</sup>19<span> / mo</span></div>
-                    <img src="assets/img/pricing-starter.png" className="img-fluid" alt="" />
-                    <ul>
-                      <li>Aida dere</li>
-                      <li>Nec feugiat nisl</li>
-                      <li>Nulla at volutpat dola</li>
-                      <li>Pharetra massa</li>
-                      <li className="na">Massa ultricies mi</li>
-                    </ul>
-                    <a href="#" className="btn-buy">Buy Now</a>
-                  </div>
-                </div>
-      
-                <div className="col-lg-3 col-md-6" data-aos="zoom-in" data-aos-delay="300">
-                  <div className="box">
-                    <h3 style="color: #ff901c;">Business Plan</h3>
-                    <div className="price"><sup>$</sup>29<span> / mo</span></div>
-                    <img src="assets/img/pricing-business.png" className="img-fluid" alt="" />
-                    <ul>
-                      <li>Aida dere</li>
-                      <li>Nec feugiat nisl</li>
-                      <li>Nulla at volutpat dola</li>
-                      <li>Pharetra massa</li>
-                      <li>Massa ultricies mi</li>
-                    </ul>
-                    <a href="#" className="btn-buy">Buy Now</a>
-                  </div>
-                </div>
-      
-                <div className="col-lg-3 col-md-6" data-aos="zoom-in" data-aos-delay="400">
-                  <div className="box">
-                    <h3 style="color: #ff0071;">Ultimate Plan</h3>
-                    <div className="price"><sup>$</sup>49<span> / mo</span></div>
-                    <img src="assets/img/pricing-ultimate.png" className="img-fluid" alt="" />
-                    <ul>
-                      <li>Aida dere</li>
-                      <li>Nec feugiat nisl</li>
-                      <li>Nulla at volutpat dola</li>
-                      <li>Pharetra massa</li>
-                      <li>Massa ultricies mi</li>
-                    </ul>
-                    <a href="#" className="btn-buy">Buy Now</a>
-                  </div>
-                </div>
-      
-              </div>
-      
-            </div>
-      
-          </section>End Pricing Section --> */}
+					{/* <PricingComponent/> */}
 
 					<section id='faq' className='faq'>
 						<div className='container' data-aos='fade-up'>
@@ -651,7 +568,6 @@ class App extends Component {
 						</div>
 					</section>
 
-<<<<<<< HEAD
 					<section id='recent-blog-posts' className='recent-blog-posts'>
 						<div className='container' data-aos='fade-up'>
 							<header className='section-header'>
@@ -680,9 +596,6 @@ class App extends Component {
 							</div>
 						</div>
 					</section>
-=======
-					{/* <BlogPosts/> */}
->>>>>>> b44924e (Added content and moved few unwanted components into another react component)
 
 					<section id='contact' className='contact'>
 						<div className='container' data-aos='fade-up'>
