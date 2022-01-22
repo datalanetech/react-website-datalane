d8f9ed9a052f1efcaf9676e1af105459d74ef56f
datalanetech <datalanetech@gmail.com>
2022-01-22 15:09:39 +0530

    Updated features component

--- src/components/FeatureTile.js
+++ src/components/FeatureTile.js
@@ -6,7 +6,7 @@ const FeatureTile = ({ description , img_src}) => {
 			<div className='feature-box d-flex align-items-center'>
 				<i className='bi bi-check'></i>
 				<h3>{description}</h3>
-				<h4>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita</h4>
+				{/* <h4>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita</h4> */}
 			</div>
 		</div>
 	);
