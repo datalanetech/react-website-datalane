c25402b9325e622794a3c2339c5339c10f8d23c1
datalanetech <datalanetech@gmail.com>
2022-01-22 14:49:40 +0530

    Added content at multiple places.

--- src/components/Values.js
+++ src/components/Values.js
@@ -8,17 +8,16 @@ class Values extends Component {
 					<div className='container' data-aos='fade-up'>
 						<header className='section-header'>
 							<h2>Our Values</h2>
-							<p>Odit est perspiciatis laborum et dicta</p>
+							<p>Datalane's 3As</p>
 						</header>
 
 						<div className='row'>
 							<div className='col-lg-4' data-aos='fade-up' data-aos-delay='200'>
 								<div className='box'>
 									<img src='assets/img/values-1.png' className='img-fluid' alt='' />
-									<h3>Ad cupiditate sed est odio</h3>
+									<h3>Autonomous</h3>
 									<p>
-										Eum ad dolor et. Autem aut fugiat debitis voluptatem consequuntur sit. Et
-										veritatis id.
+									We strive to ensure our services are autonoumous with accelerated Devops integrations.
 									</p>
 								</div>
 							</div>
@@ -26,10 +25,10 @@ class Values extends Component {
 							<div className='col-lg-4 mt-4 mt-lg-0' data-aos='fade-up' data-aos-delay='400'>
 								<div className='box'>
 									<img src='assets/img/values-2.png' className='img-fluid' alt='' />
-									<h3>Voluptatem voluptatum alias</h3>
+									<h3>Agility</h3>
 									<p>
-										Repudiandae amet nihil natus in distinctio suscipit id. Doloremque ducimus ea
-										sit non.
+									We challenge the status quo with open minds, focus, and speed.
+
 									</p>
 								</div>
 							</div>
@@ -37,10 +36,9 @@ class Values extends Component {
 							<div className='col-lg-4 mt-4 mt-lg-0' data-aos='fade-up' data-aos-delay='600'>
 								<div className='box'>
 									<img src='assets/img/values-3.png' className='img-fluid' alt='' />
-									<h3>Fugit cupiditate alias nobis.</h3>
+									<h3>Accountability</h3>
 									<p>
-										Quam rem vitae est autem molestias explicabo debitis sint. Vero aliquid quidem
-										commodi.
+									We empower each other to take ownership of our actions.
 									</p>
 								</div>
 							</div>
