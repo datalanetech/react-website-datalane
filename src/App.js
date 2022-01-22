106e230ddea59251aa9c7bc078526d8aa947fde8
datalanetech <datalanetech@gmail.com>
2022-01-22 18:29:27 +0530

    removed href warnings

--- src/App.js
+++ src/App.js
@@ -511,7 +511,7 @@ class App extends Component {
 
 				<Footer/>
 
-				<a href='#' className='back-to-top d-flex align-items-center justify-content-center'>
+				<a href='/#' className='back-to-top d-flex align-items-center justify-content-center'>
 					<i className='bi bi-arrow-up-short'></i>
 				</a>
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -32,16 +32,16 @@ const Footer = () => {
                             We deliver next generation applications, to transform your ideas into action.
                             </p>
                             <div className='social-links mt-3'>
-                                <a href='#' className='twitter'>
+                                <a href='/#' className='twitter'>
                                     <i className='bi bi-twitter'></i>
                                 </a>
-                                <a href='#' className='facebook'>
+                                <a href='/#' className='facebook'>
                                     <i className='bi bi-facebook'></i>
                                 </a>
-                                <a href='#' className='instagram'>
+                                <a href='/#' className='instagram'>
                                     <i className='bi bi-instagram'></i>
                                 </a>
-                                <a href='#' className='linkedin'>
+                                <a href='/#' className='linkedin'>
                                     <i className='bi bi-linkedin'></i>
                                 </a>
                             </div>
